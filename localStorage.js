2a8d77cd0a055f1a7459e883ea1c7d60e5b3f5d2
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-09 07:55:08 +0300

    Add descriptive comments

--- localStorage.js
+++ localStorage.js
@@ -1,10 +1,16 @@
 // Storing data:
 const myObj = { name: "John", age: 31, city: "New York" };
+//Convert myobj to JSON 
 const myJSON = JSON.stringify(myObj);
+
+// Store myJSON as "testJSON"
 localStorage.setItem("testJSON", myJSON);
 
 // Retrieving data:
 let text = localStorage.getItem("testJSON");
+
+//Convert to Js object
 let obj = JSON.parse(text);
+
 //Log output to the console 
 console.log(obj.name);
